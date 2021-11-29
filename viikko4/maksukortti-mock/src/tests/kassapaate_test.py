0a50c6f4a56b1374de33b28a20d5a8cc3c15e209
Kosti Vesterinen <kosti.vesterinen@helsinki.fi>
2021-11-29 21:35:33 +0200

    t2 tehty

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,20 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+    
+    def test_kortille_ladataan_saldo_jos_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 10
+
+        self.kassa.lataa(maksukortti_mock, 5)
+        
+        maksukortti_mock.lataa.assert_called_with(5)
+ 
+    def test_kortille_ei_ladata_saldoa_jos_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 10
+
+        self.kassa.lataa(maksukortti_mock, -5)
+        
+        maksukortti_mock.lataa.assert_not_called()
+    
